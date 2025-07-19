8569d7f687be22e7f82a96afda595798067e538b
valeriaglez-alc <valeria.alcgo@gmail.com>
2025-07-19 13:50:31 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<Login />} /> {/* ruta por defecto */}
+    </Routes>
+  );
+}
+
+export default App;
