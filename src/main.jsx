760d486ed8b3af1755fb9d1d5f0dbee3cbc2c4c7
valeriaglez-alc <valeria.alcgo@gmail.com>
2025-07-19 13:51:09 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; // ESTA LÍNEA ES NECESARIA Súper diria yo
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
